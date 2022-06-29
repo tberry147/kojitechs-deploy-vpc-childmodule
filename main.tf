472be01d8402017d9e0aafe64b306d498c0c988e
tberry147 <totimab@yahoo.com>
2022-06-29 01:25:50 -0400

    Adds new build with env

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 
 module "networking" {
 #   source = "../"
-    source = "git::https://github.com/tberry147/operational_environment_network.git?ref=v1.1.0"
+    source = "git::https://github.com/tberry147/operational_environment_network.git?ref=v1.0.0"
 
   cidr_block = var.vpc_cidr
   pub_subs   = var.public_sub
